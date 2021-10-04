efd8d1b47a3de957b2ab22622ca5be57993a70ac
Louis <louis.m.newton@hotmail.fr>
2021-10-04 09:41:28 +0200

    Modification nom et prénom du profil

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,10 +4,11 @@ namespace App\Controller;
 
 use App\Entity\Annonces;
 use App\Form\AnnoncesType;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\EditProfilType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class UserController extends AbstractController
 {
@@ -40,6 +41,28 @@ class UserController extends AbstractController
         }
 
         return $this->render('user/annonces/ajout.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/user/profil/modifier', name: 'user_profil_modifier')]
+    public function editProfil(Request $request): Response
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(EditProfilType::class, $user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            $this->addFlash('message', 'Profil mis à jour');
+            return $this->redirectToRoute("user");
+        }
+
+        return $this->render('user/editProfil.html.twig', [
             'form' => $form->createView(),
         ]);
     }
