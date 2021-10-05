f4c69e0b088549c002c8f0b6199ba28398b9c06a
Louis <louis.m.newton@hotmail.fr>
2021-10-05 12:16:45 +0200

    page détail d'une annonce

---
+++ src/Controller/AnnoncesController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\AnnoncesRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/annonces", name="annonces_")
+ * @package App/Controller
+ */
+
+class AnnoncesController extends AbstractController
+{
+    #[Route('/details/{slug}', name: 'details')]
+    public function details($slug, AnnoncesRepository $annoncesRepo): Response
+    {
+        $annonce = $annoncesRepo->findOneBy(['slug' => $slug]);
+        if(!$annonce){
+            throw new NotFoundHttpException('Pas d\'annonce trouvée');
+        }
+
+        return $this->render('annonces/details.html.twig', [
+            "annonce" => $annonce]);
+    }
+}
