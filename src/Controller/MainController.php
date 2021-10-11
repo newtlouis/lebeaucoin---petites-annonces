dc4e352ae6ffd3ca9daa2dffbc7eb96f28836f33
Louis <louis.m.newton@hotmail.fr>
2021-10-11 16:29:30 +0200

    Fonction de la recherche fulltext

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,18 +2,31 @@
 
 namespace App\Controller;
 
+use App\Form\SearchAnnonceType;
 use App\Repository\AnnoncesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(AnnoncesRepository $annoncesRepo): Response
+    public function index(AnnoncesRepository $annoncesRepo, Request $request): Response
     {
+        $annonces = $annoncesRepo->findBy(['active' => true ], ['created_at' => 'desc']);
+
+        $form = $this->createForm(SearchAnnonceType::class);
+
+        $search = $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $annonces = $annoncesRepo->search($search->get('mots')->getData());
+        }
+
         return $this->render('main/index.html.twig', [
-            'annonces' => $annoncesRepo->findBy(['active' => true ], ['created_at' => 'desc']),
+            'annonces' => $annonces,
+            'form' => $form->createView()
         ]);
     }
 }
